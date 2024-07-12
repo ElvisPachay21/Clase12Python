152dd29ed87fc4b1a9099465f7952c7befc56ecc
ElvisPachay <elvis.pachay@tecnologicoliceocristiano.edu.ec>
2024-07-11 20:17:07 -0500

    ejercicio completo

---
+++ ObtenerPromedio.py
@@ -0,0 +1,11 @@
+# Elaborar un algoritmo que
+# solicite 2 numeros y muestre
+# el promedio de ambos
+
+N1 = int(input("Ingrese el numero 1: "))
+N2 = int(input("Ingrese el numero 2: "))
+
+S = N1 + N2
+P = S / 2
+
+print(P)
